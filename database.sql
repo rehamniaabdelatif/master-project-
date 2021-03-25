640355c9593ddc5ec6cf203b9e923c069f788512
abd elatif <uabdalatif@gmail.com>
2021-03-25 11:31:39 +0100

    create database

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE mastre;
+
+USE mastre;
+
+CREATE TABLE User (
+    Personid int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UserName varchar(10) NOT NULL,
+    Pasword varchar(20) NOT NULL 
+);
+
